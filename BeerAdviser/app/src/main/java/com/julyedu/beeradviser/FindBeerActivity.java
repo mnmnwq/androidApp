354d41a81da510d829d102793d612f6a93778ffa
baiwei <baiwei@julyedu.com>
2018-07-09 20:27:04 +0800

    2222

---
+++ BeerAdviser/app/src/main/java/com/julyedu/beeradviser/FindBeerActivity.java
@@ -0,0 +1,34 @@
+package com.julyedu.beeradviser;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FindBeerActivity extends Activity {
+    private BeerExpert expert = new BeerExpert();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    //call when the user clicks the button
+    public void onClickFindBeer(View view){
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());  //得到列表项，并且转化为字串
+        //brands.setText(beerType);
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for(String brand:brandsList){
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted);
+    }
+
+}
