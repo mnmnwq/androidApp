91bb5759313c210a4ecaa829a1a43055bd35be32
baiwei <baiwei@julyedu.com>
2018-07-10 16:37:45 +0800

    22

---
+++ Stopwatch/app/src/main/java/com/mnmnwq/stopwatch/StopwatchActivity.java
@@ -0,0 +1,60 @@
+package com.mnmnwq.stopwatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class StopwatchActivity extends Activity {
+    private int seconds = 0;  //秒数
+    private boolean running;  //控制运行
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+        if(savedInstanceState != null){  //保留状态
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+        }
+        runTimer();
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putInt("seconds",seconds);
+        savedInstanceState.putBoolean("running",running);
+    }
+
+    public void onClickStart(View view){
+        running = true;
+    }
+    public void onClickStop(View view){
+        running = false;
+    }
+    public void onClickReset(View view){
+        seconds = 0;
+        running = false;
+    }
+
+    private void runTimer(){
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format("%d:%02d:%02d",hours,minutes,secs);  //格式化 java代码
+                timeView.setText(time);
+                if(running){
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
+
+    }
+}
