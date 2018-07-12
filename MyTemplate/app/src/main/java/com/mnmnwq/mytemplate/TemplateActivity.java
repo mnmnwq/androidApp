6bedff7abd769a378018b5744c7df89a67d88c69
baiwei <baiwei@julyedu.com>
2018-07-12 16:25:46 +0800

    2222

---
+++ MyTemplate/app/src/main/java/com/mnmnwq/mytemplate/TemplateActivity.java
@@ -0,0 +1,13 @@
+package com.mnmnwq.mytemplate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TemplateActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_template);
+    }
+}
